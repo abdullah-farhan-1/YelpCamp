27f32d29a5d1dacc1107d82ead0dd2734651ed3e
Abdullah Farhan <abdullah.farhan.uet@gmail.com>
2024-09-19 23:03:26 +0500

    app.js modified

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const helmet=require('helmet')
 const MongoStore=require('connect-mongo')
 
 
-const dbUrl='mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl=process.env.DB_URL||'mongodb://127.0.0.1:27017/yelp-camp'
 const connection=() => {
     try {
         mongoose.connect(dbUrl);
